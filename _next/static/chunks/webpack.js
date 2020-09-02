93ab637d2f093623e448393c708469ec72b7b26c
mohan <mohan.krishna4220@gmail.com>
2020-09-02 14:12:37 +0530

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a24a3aa25467fb7b7b8d";
+/******/ 	var hotCurrentHash = "919dd0828bb47f05df37";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/w6fjHFT4ynpYwvOEU8qOU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fc05e8dab40ca9a265383.css","static\u002Fchunks\u002Fpages\u002Findex-66626dde58f1783f566f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
